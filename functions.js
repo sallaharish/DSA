1cfdbad68af798275ad254a5b2dfc9db2f7bbf5b
Salla Harish <sallaharish040@gmail.com>
2025-07-15 14:58:51 +0530

    first commit

---
+++ functions.js
@@ -0,0 +1,23 @@
+// /create the function declearatoio
+// use the if else conditon inorde rto check the age of the person and then retuent he string
+// for checking the age of the person 
+function checkage(age) {
+    if (age > 18) {
+        return ("egligible for vote")
+    }
+    return "not agligible"
+
+}
+
+
+console.log(checkage(12))
+
+// for checking the number is even or odd
+function evenorodd(num) {
+    if (num % 2 == 0) {
+        return "even"
+    }
+    return "odd"
+}
+
+console.log(evenorodd(1))
