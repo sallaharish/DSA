8d2bf7fac35885a3f7e5dd5c09f0665ae34bbc9c
Salla Harish <sallaharish040@gmail.com>
2025-07-16 15:06:34 +0530

    loop insde the loop double loop

---
+++ doubleloop.js
@@ -0,0 +1,23 @@
+for (let i = 0; i < 5; i++){
+    for (let j = 0; j < 4; j++){
+        console.log(i,j)
+    }
+}
+
+for (let i = 0; i <= 3; i++){
+    for (let j = 0; j < i; j++){
+        console.log(i,j)
+    }
+}
+
+for (let i = 0; i < 3; i++){
+    for (let j = i; j > 0; j--){
+        console.log(i,j)
+    }
+}
+
+for (i = 5; i > 0; i--){
+    for (let j = 0; j < i; j++){
+        console.log(i,j)
+    }
+}
